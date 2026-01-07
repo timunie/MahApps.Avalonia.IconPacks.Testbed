a6e5b0e1af04894e96f7177c5d54cc69d451eb0f
timunie <tim.uniewski@gmail.com>
2026-01-07 12:32:50 +0100

    We can safely remove logging at this stage
    There wasn't much meaningful logging anyways.

--- src/MahApps.IconPacksBrowser.Avalonia.Desktop/Program.cs
+++ src/MahApps.IconPacksBrowser.Avalonia.Desktop/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using Avalonia;
-using Serilog;
 
 namespace MahApps.IconPacksBrowser.Avalonia.Desktop;
 
@@ -15,37 +11,14 @@ sealed class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        ConfigureLogging();
-
-        AppDomain.CurrentDomain.UnhandledException += (s, e) =>
-            Log.Fatal(e.ExceptionObject as Exception, "[FATAL] Unhandled exception in AppDomain");
-
-        TaskScheduler.UnobservedTaskException += (s, e) =>
-        {
-            Log.Error(e.Exception, "[ERROR] Unobserved task exception");
-            e.SetObserved();
-        };
-
         try
         {
-            Log.Information("Starting Avalonia app (AOT={AOT})",
-#if NET8_0_OR_GREATER
-                System.Runtime.CompilerServices.RuntimeFeature.IsDynamicCodeCompiled == false
-#else
-                false
-#endif
-            );
-
             BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
         }
         catch (Exception ex)
         {
-            Log.Fatal(ex, "Application terminated unexpectedly during startup");
+            Console.WriteLine("Application terminated unexpectedly during startup");
             throw;
-        }
-        finally
-        {
-            Log.CloseAndFlush();
         }
     }
 
@@ -60,39 +33,4 @@ sealed class Program
                 MahApps.IconPacksBrowser.Avalonia.Services.SettingsStorage.Register(new MahApps.IconPacksBrowser.Avalonia.Desktop.Services.DesktopSettingsStorageService());
             })
             .LogToTrace();
-
-    private static void ConfigureLogging()
-    {
-        try
-        {
-            var baseDir = AppContext.BaseDirectory;
-            var logDir = Path.Combine(baseDir, "logs");
-            Directory.CreateDirectory(logDir);
-            var logPath = Path.Combine(logDir, "app-.log");
-
-            var cfg = new LoggerConfiguration()
-#if DEBUG
-                .MinimumLevel.Debug()
-#else
-                .MinimumLevel.Information()
-#endif
-                .WriteTo.File(
-                    logPath,
-                    rollingInterval: RollingInterval.Day,
-                    retainedFileCountLimit: 7,
-                    shared: true,
-                    flushToDiskInterval: TimeSpan.FromSeconds(1),
-                    buffered: false,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
-                ;
-
-            Log.Logger = cfg.CreateLogger();
-            // Route Avalonia Trace logs into Serilog
-            Trace.Listeners.Add(new SerilogTraceListener.SerilogTraceListener());
-        }
-        catch
-        {
-            // Last resort: avoid crashing if logging setup fails under AOT
-        }
-    }
 }
